563fab77835968d0616da12c5d6c7661736f8466
Thrumbo33 <vladislavyancenko924@gmail.com>
2025-03-25 09:23:40 +0100

    page

--- frontend/app/dashboard/loans/page.js
+++ frontend/app/dashboard/loans/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Home from "../../../components/dashcomp/Loans/LoansPage";
 import "../../../styles/Navigation.css";
 import "../../../styles/NavDash.css";
