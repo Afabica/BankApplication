6b8067037b332dc498c337e215df2b5e5314976b
Thrumbo33 <vladislavyancenko924@gmail.com>
2025-03-25 09:28:44 +0100

    page

--- frontend/app/dashboard/investment/page.js
+++ frontend/app/dashboard/investment/page.js
@@ -1,12 +1,10 @@
-"use client";
-
-import React from 'react';
+import React from "react";
 import InvestmentPage from "../../../components/dashcomp/Investment/InvestPage";
 
 const InvestPage = () => {
   <div>
-    <InvestmentPage/>
-    </div>
-}
+    <InvestmentPage />
+  </div>;
+};
 
 export default InvestPage;
--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Home from "../../components/dashcomp/MainPage/ProfPage";
 import "../../styles/Promotions.css";
 //import '../../styles/global.css'
